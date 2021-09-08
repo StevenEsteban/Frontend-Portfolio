8569090aa88aecf35a610c51abc5c73758a8afeb
Steven Esteban <stevenesteban.g@gmail.com>
2021-09-08 15:37:26 +0200

    added functionnal mail form

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import {Component} from "react";
 import Navbar from '../Navbar';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
-class Contact extends Component {
 
-    render(){
-    
-      return( 
+function Contact() {
+ const [toSend, setToSend] = useState ({
+        from_name:'',
+        to_name:'',
+        message:'',
+        reply_to:'',
+      });
 
-        <section class="bg--secondary-color pt3 pb3">
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+          'service_fx01jpa',
+          'template_1grr6i8',
+          toSend,
+          'user_glocIcRowhRyBqPWbUgo7'
+        )
+        .then((response) => {
+          console.log ('message sent !', response.status, response.text);
+        })
+        .catch((err) => {
+          console.log('failed to send your message', err);
+        })
+      }
+
+      const handleChange = (e) => {
+        setToSend({...toSend, [e.target.name]: e.target.value});
+      };
+
+     return( 
+
+        <div className="SendForm">
+      <section class="bg--secondary-color pt3 pb3">
         <div class="container">
             <div class="mb2 align--center">
                 <h3 class="mb1">Contact me to get started</h3>
@@ -16,31 +44,28 @@ class Contact extends Component {
             </div>
 
 
-            <form>
+            <form onSubmit={onSubmit}>
                 <div class="grid-row">
-                    <div class="grid-column span-half mt1">
+                    <div class="grid-column mt1">
                         <label class="label" for="name">Name</label>
-                        <input class="form-control" id="name"/>
+                        <input name="from_name" value={toSend.from_name} onChange={handleChange} class="form-control" id="name" />
                     </div>
-                    <div class="grid-column span-half mt1">
+                    {/* <div class="grid-column span-half mt1">
                         <label class="label" for="email">Email</label>
-                        <input class="form-control" id="email" type="email"/>
-                    </div>
+                        <input name="to_name" value={toSend.to_name} onChange={handleChange} class="form-control" id="email"/>
+                    </div> */}
                 </div>
-                <label class="label mt1" for="dates">Company Name (optional)</label>
-                <input class="form-control" id="dates"/>
+                <label class="label mt1" for="dates">Email</label>
+                <input name="reply_to" value={toSend.reply_to} onChange={handleChange} class="form-control" id="dates"/>
                 <label class="label mt1" for="message">Message</label>
-                <textarea class="form-control" id="message"></textarea>
+                <textarea type="text" name="message" value={toSend.message} onChange={handleChange} class="form-control" id="message"></textarea>
                 <div class='submitButton'>
-                      <button class="btn btn--outline">Send</button>
+                      <button type="submit" class="btn btn--outline">Send</button>
                 </div>
             </form>
         </div>
     </section>
-      )}}
-    
-  
-  
-  
-  
-  export default Contact
+     </div>
+        
+      )}
+export default Contact
