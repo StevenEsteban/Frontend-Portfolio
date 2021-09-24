4831c93ff279d969cb318956d4ec9f3ca4bc89bd
Steven Esteban <stevenesteban.g@gmail.com>
2021-09-24 18:23:32 +0200

    fixed margins

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,156 +1,215 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 
 
 class Home extends Component {
 
-    render(){
-    
-      return( 
-          <body>
-              
-         
-<div>
-    <div className="headBackground">
-    
-<section className="SectionHome">
+    render() {
 
-<div className="TitleName">
-<h1>Steven Esteban Gonzalez</h1>
-<h3>Hi, I'm a junior web developer. <br/> Be my guest and make sure to check out my work !</h3>
-</div>
-<div className=""><Link to="/PreviousWork"><button class="btn1">Check out my work</button></Link></div>
-
-</section>
-</div>
-<div class="has-animations">
+        return ( <
+            body >
 
 
+            <
+            div >
+            <
+            div className = "headBackground" >
 
-    <header class="pt2 pb1 align--center">
-        <div class="container">
-            
-            <h1 className="about">About Me</h1>
-            <br/>
-<div className="aboutMe">
-    <h4>
-After a career change, I was eager to enter the world
-of web development. Since then, my main goal is to
-improve my IT skills and discover new web
-technologies everyday.
-More focused on the Frontend at the moment, I am
-willing to increase my knowledge on both Front- and
-Backend.</h4></div>
-        
-        </div>
-    </header>
+            <
+            section className = "SectionHome" >
 
+            <
+            div className = "TitleName" >
+            <
+            h1 > Steven Esteban Gonzalez < /h1> <
+            h3 > Hi, I 'm a junior web developer. <br/> Be my guest and make sure to check out my work !</h3> <
+            /div> <
+            div className = "btnSectionHome" > < Link to = "/PreviousWork" > < button class = "btn1" > Check out my work < /button></Link > < /div>
 
-    <main>
-
-        <div class="container">
+            <
+            /section> <
+            /div> <
+            div class = "has-animations" >
 
 
 
-            <section class="grid-row grid-row--center">
+            <
+            header class = "pt2 pb1 align--center" >
+            <
+            div class = "container" >
 
-                {/* <div class="grid-column span-half pt3 pb3 mobile-m order-1 reveal-on-scroll is-revealing">
-                    <div class="relative">
-                        
-                      
-                        <img src="/img/Web.png" alt="Cabin"/>
-                     
-                    </div>
-                </div> */}
-                {/* <div class="grid-column span-half pt3 pb3 mobile-m order-2">
-                    <h3>Get the right estimation</h3>
-                    <p className="paragraphes">Our greatest experts gathered their knowledge to give you the most precise estimation for your future home.Thanks to the form below, you'll get an idea of what your future dreamhouse might cost you.</p>
-                </div> */}
+            <
+            h1 className = "about" > About Me < /h1> <
+            br / >
+            <
+            div className = "aboutMe" >
+            <
+            h4 >
+            After a career change, I was eager to enter the world of web development.Since then, my main goal is to improve my IT skills and discover new web technologies everyday.More focused on the Frontend at the moment, I am willing to increase my knowledge on both Front - and Backend. < /h4></div >
 
-                <div class="grid-column span-half pt3 pb3 mobile-m order-4">
-                    <h3>As fast as the API can load</h3>
-                    <p className="paragraphes">In just a few seconds (normally) you'll be able to estimate any type of house you can think about. <br/> Everything starts with a price prediction. </p>
-                </div>
-                <div class="grid-column span-half pt3 pb3 mobile-m order-3 reveal-on-scroll is-revealing">
-                    <div class="relative2">
-                 
-                        <img className="imageOrdi" src="/img/computers.png" alt="Teepee"/>
-                       
-                    </div>
-                </div>
+            <
+            /div> <
+            /header>
 
-                <div class="grid-column span-half pt3 pb3 mobile-m order-5 reveal-on-scroll is-revealing">
-                    <div class="relative">
-                   
-                        <img class="info-image relative z2 image" src="img/macbook.png" alt="Fishing"/>
-                     
-                    </div>
-                </div>
-                <div class="grid-column span-half pt3 pb3 mobile-m order-6">
-                    <h3>Get in touch </h3>
-                    <p className="paragraphes">Want to buy or sell your house ? <br/>Then make sure to fill in the contact form below to reach us out. Our experts will do their best to find your new house that matches all your expectations. </p>
-                </div>
 
-            </section>
+            <
+            main >
 
-        </div>
+            <
+            div class = "container" >
 
 
 
+            <
+            section class = "grid-row grid-row--center" >
 
-    </main>
-    <div id="root"></div>
-    <section class="align--center pt2 pb3">
-        <p class="WanTo">Want to create something together ?</p>
-        <div className=""><Link to="/Contact"><button class="btn1">Reach me out !</button></Link></div>
+            {
+                /* <div class="grid-column span-half pt3 pb3 mobile-m order-1 reveal-on-scroll is-revealing">
+                                    <div class="relative">
+                                        
+                                      
+                                        <img src="/img/Web.png" alt="Cabin"/>
+                                     
+                                    </div>
+                                </div> */
+            } {
+                /* <div class="grid-column span-half pt3 pb3 mobile-m order-2">
+                                    <h3>Get the right estimation</h3>
+                                    <p className="paragraphes">Our greatest experts gathered their knowledge to give you the most precise estimation for your future home.Thanks to the form below, you'll get an idea of what your future dreamhouse might cost you.</p>
+                                </div> */
+            }
 
-    </section>
-   
-   
+            <
+            div class = "grid-column span-half pt3 pb3 mobile-m order-4" >
+            <
+            h3 > As fast as the API can load < /h3> <
+            p className = "paragraphes" > In just a few seconds(normally) you 'll be able to estimate any type of house you can think about. <br/> Everything starts with a price prediction. </p> <
+            /div> <
+            div class = "grid-column span-half pt3 pb3 mobile-m order-3 reveal-on-scroll is-revealing" >
+            <
+            div class = "relative2" >
 
-    <footer class="pt2 pb2">
-        <div class="container align--center">
-            <p class="adresse">Cantersteen 10, 1000 Bruxelles</p>
+            <
+            img className = "imageOrdi"
+            src = "/img/computers.png"
+            alt = "Teepee" / >
 
-            <ul class="no-bullets list--inline mt1">
-                <li class="mr1">
-                    <a href="https://www.facebook.com/steven.estebangonzalez/" class="link"><img class="icon" src="/img/facebook.svg" alt="Facebook"/></a>
-                </li>
-                <li class="mr1">
-                    <a href="https://www.instagram.com/steven_esteban/" class="link"><img class="icon" src="/img/instagram.svg" alt="Instagram"/></a>
-                </li>
-                 <li class="mr1">
-                    <a href="https://www.linkedin.com/in/steven-esteban-gonzalez-/" class="link"><img class="icon2" src="/img/linkedin.svg" alt="Linkedin"/></a>
-                </li>
-                <li class="mr1">
-                    <a href="https://github.com/StevenEsteban" class="link"><img class="icon2" src="/img/iconmonstr-github-1.svg" alt="Github"/></a>
-                </li>
-            </ul>
-        </div>
-    </footer>
+            <
+            /div> <
+            /div>
 
+            <
+            div class = "grid-column span-half pt3 pb3 mobile-m order-5 reveal-on-scroll is-revealing" >
+            <
+            div class = "relative" >
 
-    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+            <
+            img class = "info-image relative z2 image"
+            src = "img/macbook.png"
+            alt = "Fishing" / >
 
+            <
+            /div> <
+            /div> <
+            div class = "grid-column span-half pt3 pb3 mobile-m order-6" >
+            <
+            h3 > Get in touch < /h3> <
+            p className = "paragraphes" > Want to buy or sell your house ? < br / > Then make sure to fill in the contact form below to reach us out.Our experts will do their best to find your new house that matches all your expectations. < /p> <
+                /div>
 
-    <script src="js/production.js"></script>
+            <
+            /section>
+
+            <
+            /div>
 
 
 
 
+            <
+            /main> <
+            div id = "root" > < /div> <
+            section class = "align--center pt2 pb3" >
+            <
+            p class = "WanTo" > Want to create something together ? < /p> <
+            div className = "" > < Link to = "/Contact" > < button class = "btn1" > Reach me out! < /button></Link > < /div>
+
+            <
+            /section>
 
 
-</div>
 
-</div>
+            <
+            footer class = "pt2 pb2" >
+            <
+            div class = "container align--center" >
+            <
+            p class = "adresse" > Cantersteen 10, 1000 Bruxelles < /p>
 
-</body>
+            <
+            ul class = "no-bullets list--inline mt1" >
+            <
+            li class = "mr1" >
+            <
+            a href = "https://www.facebook.com/steven.estebangonzalez/"
+            class = "link" > < img class = "icon"
+            src = "/img/facebook.svg"
+            alt = "Facebook" / > < /a> <
+            /li> <
+            li class = "mr1" >
+            <
+            a href = "https://www.instagram.com/steven_esteban/"
+            class = "link" > < img class = "icon"
+            src = "/img/instagram.svg"
+            alt = "Instagram" / > < /a> <
+            /li> <
+            li class = "mr1" >
+            <
+            a href = "https://www.linkedin.com/in/steven-esteban-gonzalez-/"
+            class = "link" > < img class = "icon2"
+            src = "/img/linkedin.svg"
+            alt = "Linkedin" / > < /a> <
+            /li> <
+            li class = "mr1" >
+            <
+            a href = "https://github.com/StevenEsteban"
+            class = "link" > < img class = "icon2"
+            src = "/img/iconmonstr-github-1.svg"
+            alt = "Github" / > < /a> <
+            /li> <
+            /ul> <
+            /div> <
+            /footer>
 
-      )}}
-    
-  
-  
-  
-  
-  export default Home
+
+            <
+            script src = "https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js" > < /script>
+
+
+            <
+            script src = "js/production.js" > < /script>
+
+
+
+
+
+
+            <
+            /div>
+
+            <
+            /div>
+
+            <
+            /body>
+
+        )
+    }
+}
+
+
+
+
+
+export default Home
